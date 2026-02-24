ea2fa7bcaf9fc388a46b284020bbf227a927d7e4
RUBKO <65393289+RUBKO@users.noreply.github.com>
2026-02-24 18:22:03 +0000

    Update app.java

--- app.java
+++ app.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven World today & me!");
+        System.out.println("Hello, Maven World today!");
     }
 }
